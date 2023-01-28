4e7cbea31339a87dee2e8d92c33a6cb16b55ce95
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2023-01-28 17:42:58 +0100

    Add files via upload

---
+++ bikeroll/database/migrations/2023_01_28_133728_create_ensures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnsuresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ensures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_insurances');
+            $table->unsignedBigInteger('id_runner');
+            $table->integer('price');
+
+            //Foreign keys
+            $table->foreign('id_insurances')->references('id')->on('insurances');
+            $table->foreign('id_runner')->references('id')->on('runners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ensures');
+    }
+}
