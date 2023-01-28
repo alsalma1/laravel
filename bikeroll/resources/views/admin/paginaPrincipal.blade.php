abe6764f06876e5f3fc6c7297ca925a9629aba1e
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2023-01-28 17:45:00 +0100

    Add files via upload

--- bikeroll/resources/views/admin/paginaPrincipal.blade.php
+++ bikeroll/resources/views/admin/paginaPrincipal.blade.php
@@ -3,23 +3,32 @@
 <div style="border:1px solid;width:20%;margin:auto">
     <div id="div1" style="border:1px solid;width:100%;text-align:center">
         <h3>Gestionar carreras</a></h3>
+        <p><a href="">Mostrar todos</a></p>
         <p><a href="">Añadir carrera</a></p>
         <p><a href="">Editar carrera</a></p>
         <p><a href="">Desactivar carrera</a></p>
     </div>
     
     <div style="border:1px solid;width:100%;text-align:center">
-        <h3>Gestionar aseguradores</h3>
-        <p><a href="{{url('anyadirAsegurador')}}">Añadir asegurador</a></p>
-        <p><a href="{{url('editarAsegurador')}}">Editar asegurador</a></p>
-        <p><a href="{{url('desactivarAsegurador')}}">Desactivar asegurador</a></p>
+        <h3>Gestionar aseguradoras</h3>
+        <p><a href="{{url('mostrarTodosAs')}}">Mostrar todos</a></p>
+        <p><a href="{{url('anyadirAseguradora')}}">Añadir aseguradora</a></p>
+        <p><a href="{{url('editarAsegurador')}}">Editar aseguradora</a></p>
+        <p><a href="{{url('desactivarAsegurador')}}">Desactivar aseguradora</a></p>
     </div>
     
     <div style="border:1px solid;width:100%;text-align:center">
         <h3>Gestionar sponsors</h3>
+        <p><a href="">Mostrar todos</a></p>
         <p><a href="">Añadir sponsor</a></p>
         <p><a href="">Editar sponsor</a></p>
         <p><a href="">Desactivar sponsor</a></p>
     </div>
 </div>
 
+<h3><a href="{{url('/')}}">Cerrar sesión</a></h3>
+
+@foreach ($admin as $item)
+    <p>{{$item['user_name']}}</p>
+    
+@endforeach
