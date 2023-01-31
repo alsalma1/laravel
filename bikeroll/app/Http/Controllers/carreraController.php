189af6a965cf2fecc229d1bf19cacdf57d583e63
Diana <113168737+diana120500@users.noreply.github.com>
2023-01-31 12:42:04 +0100

    Add files via upload

--- bikeroll/app/Http/Controllers/carreraController.php
+++ bikeroll/app/Http/Controllers/carreraController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class carreraController extends Controller
 {
-    //
+    public function addRace(){
+        return view('admin.carreras.anyadirCarrera');
+        // if ($_POST['send']{
+
+        // })
+    }
+
+
+    public function editRace(){
+        return view('admin.carreras.editarCarrera');
+
+    }
+
+    public function deleteRace(){
+        return view('admin.carreras.desactivarCarrera');
+
+    }
 }
