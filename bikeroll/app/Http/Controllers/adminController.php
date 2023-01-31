1c9c2430aafeb9df1d3acb2cdc0badb0431962d8
Diana <113168737+diana120500@users.noreply.github.com>
2023-01-31 09:39:30 +0100

    Add files via upload

--- bikeroll/app/Http/Controllers/adminController.php
+++ bikeroll/app/Http/Controllers/adminController.php
@@ -7,19 +7,21 @@ use Illuminate\Http\Request;
 class adminController extends Controller
 {
     //Crear un nuevo registro
-    public function create(){
+    public function create(Request $request){
         $admin = new Admin();
-        $user_name = $admin->user_name = 'admin1';
-        //Si existe la clave primaria
-        if (Admin::where('user_name', $user_name )->exists()) {
-            echo "ya existe";
-        }
-        else{
-            $user_name = $admin->user_name = 'admin1';
-            $admin->passwd = 'a';
-            $admin;
-            $admin->save();
-            echo "no existe";
+        if (isset($_POST['send'])){
+            $user_name = $admin->user_name = $_POST['email'];
+            //Si existe la clave primaria
+            if (Admin::where('user_name', $user_name )->exists()) {
+                echo "ya existe";
+            }
+            else{
+                $user_name = $admin->user_name = $_POST['email'];
+                $admin->passwd = $_POST['passwd'];
+                $admin;
+                $admin->save();
+                echo "no existe";
+            }
         }
     }
 }
