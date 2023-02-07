4cf776b96c6ea8e2bd5cfbd9deef3cdfa01d36df
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2023-02-07 22:52:25 +0100

    Add files via upload

--- bikeroll/resources/views/admin/paginaPrincipal.blade.php
+++ bikeroll/resources/views/admin/paginaPrincipal.blade.php
@@ -13,22 +13,13 @@
         <h3>Gestionar aseguradoras</h3>
         <p><a href="{{url('mostrarTodosAs')}}">Mostrar todos</a></p>
         <p><a href="{{url('anyadirAseguradora')}}">Añadir aseguradora</a></p>
-        <p><a href="{{url('editarAsegurador')}}">Editar aseguradora</a></p>
-        <p><a href="{{url('desactivarAsegurador')}}">Desactivar aseguradora</a></p>
     </div>
     
     <div style="border:1px solid;width:100%;text-align:center">
         <h3>Gestionar sponsors</h3>
-        <p><a href="">Mostrar todos</a></p>
-        <p><a href="">Añadir sponsor</a></p>
-        <p><a href="">Editar sponsor</a></p>
-        <p><a href="">Desactivar sponsor</a></p>
+        <p><a href="{{url('mostrarSponsors')}}">Mostrar todos</a></p>
+        <p><a href="{{url('anyadirSponsor')}}">Añadir sponsor</a></p>
     </div>
 </div>
 
 <h3><a href="{{url('/')}}">Cerrar sesión</a></h3>
-
-{{-- @foreach ($admin as $item)
-    <p>{{$item['user_name']}}</p>
-    
-@endforeach --}}
