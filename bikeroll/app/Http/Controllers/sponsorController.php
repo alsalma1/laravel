e3151a5a6628188d957628b380520518169cc51e
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2023-02-08 21:08:10 +0100

    Update sponsorController.php

--- bikeroll/app/Http/Controllers/sponsorController.php
+++ bikeroll/app/Http/Controllers/sponsorController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\Sponsor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class sponsorController extends Controller
 {
@@ -21,10 +23,75 @@ class sponsorController extends Controller
                 'logo' => $request->file('logo'),
                 'main_plain' => $request->input('pp')
             ]);
+            
+            //subir la imagen
+            if($request->hasFile('logo')){
+                $imagen = $request->file('logo');
+
+                //aquí le asignamos el nombre
+                $nombreimagen = Str::slug($request->file('logo')).".".$imagen->guessExtension();
+
+                //y la ruta
+                $ruta = public_path("../resources/img/");
+
+                //$imagen->move($ruta,$nombreimagen);
+                copy($imagen->getRealPath(),$ruta.$nombreimagen);        
+                
+            }
             $sponsor->save();
+            return redirect()->route('mostrarSponsors');
         }        
         else{
             return view('admin.sponsors.anyadirSponsor');
+        }
+    }
+
+    public function activate(Request $request){
+        $id = $request->id;
+        $sponsor = Sponsor::find($id);
+        $estado = $sponsor->sponsorState;
+        if($estado==1){
+            $estado = $sponsor->sponsorState = 0;
+            echo $estado;
+        }
+        else{
+            $sponsor->sponsorState = 1;
+        }
+        $sponsor->save();
+        return redirect('mostrarSponsors');   
+    }
+
+    public function edit(Request $request){
+        $id = $request->id;
+        $sponsor = Sponsor::find($id);
+        //Si se envian datos , modifica el registro
+        if ($request->isMethod('post')){
+            $sponsor->name = $request->input('name');
+            $sponsor->description = $request->input('desc');
+            $sponsor->main_plain = $request->input('pp');
+            $sponsor->save();
+            echo $sponsor['main_plain'];
+            return redirect('mostrarSponsors');
+        }      
+        else{
+            return view('admin.sponsors.editarSponsor' ,[
+                'sponsor' => $sponsor
+            ]);
+        }
+    }
+
+    public function editLogo(Request $request){
+        $id = $request->id;
+        $sponsor = Sponsor::find($id);
+        if ($request->isMethod('post')){
+            $sponsor->logo = $request->file('logo');
+            $sponsor->save();
+            return redirect('mostrarSponsors');
+        }      
+        else{
+            return view('admin.sponsors.editarLogo' ,[
+                'sponsor' => $sponsor
+            ]);
         }
     }
 }
