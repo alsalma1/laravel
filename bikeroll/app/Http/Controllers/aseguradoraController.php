fc15dc85323e3360a6b0bcdb0c716e4272e62403
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2023-02-08 21:07:45 +0100

    Update aseguradoraController.php

--- bikeroll/app/Http/Controllers/aseguradoraController.php
+++ bikeroll/app/Http/Controllers/aseguradoraController.php
@@ -7,77 +7,97 @@ use App\Models\Insurance;
 class aseguradoraController extends Controller
 {
     public function index(){
-        //Mostrar todas las aseguradoras
-        $insurance = Insurance::all();
-        return view('admin.aseguradoras.mostrarTodosAs' , [
-            'insurance' => $insurance
-        ]);
+        if(isset($_SESSION['admin'])){
+            //Mostrar todas las aseguradoras
+            $insurance = Insurance::all();
+            return view('admin.aseguradoras.mostrarTodosAs' , [
+                'insurance' => $insurance
+            ]);
+        }
+        else{
+            $this->notLogin();
+        }
     }
 
     //Añadir nueva aseguradora
     public function create(Request $request){
-        $insurance = new Insurance();
-        if(isset($_POST['create'])){
-            $cif = $insurance->CIF = $_POST['cif'];
-            //Si existe la clave primaria
-            if (Insurance::where('CIF', $cif)->exists()) {
-                echo "ya existe esta aseguradora";
-                return redirect('/paginaPrincipal');
-            }
+        if(isset($_SESSION['admin'])){
+            $insurance = new Insurance();
+            if(isset($_POST['create'])){
+                $cif = $insurance->CIF = $_POST['cif'];
+                //Si existe la clave primaria
+                if (Insurance::where('CIF', $cif)->exists()) {
+                    ?><script>alert("ya existe esta aseguradora!")</script><?php
+                    return redirect('anyadirAseguradora');
+                }
+                else{
+                    $insurance->name = $_POST['insuranceName'];
+                    $insurance->address = $_POST['insuranceAdress'];
+                    $insurance->save();
+                    ?><script>alert("Se ha creado correctamente!")</script><?php
+                    return redirect('mostrarTodosAs');
+                }
+            }        
             else{
-                $insurance->name = $_POST['insuranceName'];
-                $insurance->address = $_POST['insuranceAdress'];
-                $insurance->save();
-                echo "se ha creado correctamente";
-                redirect('/paginaPrincipal');
-                // return view('admin.aseguradoras.editarAseguradora' , [
-                //     'insurance' => $insurance
-                // ]);
+                return view('admin.aseguradoras.anyadirAseguradora');
             }
-        }        
+        }
         else{
-            return view('admin.aseguradoras.anyadirAseguradora');
+            $this->notLogin();
         }
     }
 
     public function activate(Request $request){
-        $id = $request->id;
-        $insurance = Insurance::find($id);
-        $estado = $insurance->estado;
-        if($estado==1){
-            $estado = $insurance->estado = 0;
-            echo $estado;
-        }
-        else{
-            $insurance->estado = 1;
-        }
-        $insurance->save();
-        $insurance = Insurance::all();
-        
-        return redirect()->route('mostrarTodosAs' , [
-            'insurance'=>$insurance
-        ]);   
-    }
-    public function edit(Request $request){
-        $id = $request->id;
-        //Formulario para modificar una aseguradora
-        $insurance = Insurance::find($id);
-        //Si se envian datos , modifica el registro
-        if ($request->isMethod('post')){
-            $insurance->CIF = $request->input('cif');
-            $insurance->name = $request->input('insuranceName');
-            $insurance->address = $request->input('insuranceAdress');
+        if(isset($_SESSION['admin'])){
+            $id = $request->id;
+            $insurance = Insurance::find($id);
+            $estado = $insurance->estado;
+            if($estado==1){
+                $estado = $insurance->estado = 0;
+                echo $estado;
+            }
+            else{
+                $insurance->estado = 1;
+            }
             $insurance->save();
             $insurance = Insurance::all();
-            return redirect()->route('mostrarTodosAs' , [
-                'insurance'=>$insurance
-            ]); 
-        }      
+            
+            return redirect('mostrarTodosAs');  
+        }
         else{
-            return view('admin.aseguradoras.editarAseguradora' ,[
-                'insurance' => $insurance
-            ]);
+            $this->notLogin();
         }
     }
-}
 
+    public function edit(Request $request){
+        if(isset($_SESSION['admin'])){
+            $id = $request->id;
+            //Formulario para modificar una aseguradora
+            $insurance = Insurance::find($id);
+            //Si se envian datos , modifica el registro
+            if ($request->isMethod('post')){
+                $insurance->CIF = $request->input('cif');
+                $insurance->name = $request->input('insuranceName');
+                $insurance->address = $request->input('insuranceAdress');
+                $insurance->save();
+                $insurance = Insurance::all();
+                return redirect('mostrarTodosAs');
+            }      
+            else{
+                return view('admin.aseguradoras.editarAseguradora' ,[
+                    'insurance' => $insurance
+                ]);
+            }
+        }
+        else{
+            $this->notLogin();
+        }
+    }
+
+    public function notLogin(){
+        ?>
+        <script>alert("Inicia sesión para ver esta página!")</script>
+        <?php
+        return redirect('/');
+    }
+}
